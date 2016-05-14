a3debc4dd0e0793b846643d84d73be3ee2f7f5f3
FortalezaRN <fortaleza555@hotmail.com>
2016-05-14 15:19:58 -0300

    Brincando com Angular

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+	webserver = require('gulp-webserver');
+
+gulp.task('serve', function() {
+	gulp.src('app')
+		.pipe(webserver({
+			livereload: true,
+			open: true,
+			directoryListerning: true
+		}));
+
+});
